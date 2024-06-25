2ece8a2e3923963a1fc1c1015575c98a04fbff72
alfinroy05 <alfinroy2005@gmail.com>
2024-06-25 21:40:00 +0530

    day 2

--- javascript/sscript.js
+++ javascript/sscript.js
@@ -1,8 +1,72 @@
-console.log("Hello world");
-console.warn("hmmm");
-console.error("kkkkk");
+console.log("hello world");
+console.warn("hello world");
+console.error("hello world");
 
-
-
-var a = 90;
+var a = 10;
 console.log(a);
+var a = "name";
+console.log(a);
+
+
+let name = "john";
+console.log(name);
+
+
+const pi = 3.14;
+console.log(pi);
+
+
+
+let valueOne = 2;
+let valueTwo = "a";
+
+console.log(valueOne + valueTwo);
+
+console.log(valueOne + parseInt(valueTwo));
+
+
+const nam = "john";
+const age=30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+var y;
+
+console.log(typeof name);
+console.log('my name is ' + name + ' and i am '+ age);
+console.log('my name is ${nam} and i am ${age}');
+
+
+// // array
+arrayOne = ["john",30,true,null,undefined]
+
+console.log(arrayOne);
+ 
+// // push
+
+arrayOne.push("apple");
+console.log(arrayOne);
+
+
+//pop
+
+arrayOne.pop();
+console.log(arrayOne);
+
+//unshift
+
+arrayOne.unshift("mango");
+console.log(arrayOne);
+
+console.log(arrayOne[3]);
+
+arrayOne[3]="bananana";
+console.log(arrayOne);
+
+console.log(arrayOne.length);
+arrayOne.splice(3,2);
+console.log(arrayOne);
+
+
+
+
